fc4c208ef91ed73e1f5b4f71fe121074b95cf641
yutamatsumura <yuta.matsumura@bravi.co.jp>
2017-07-24 04:36:47 +0000

    top & login

---
+++ resources/views/commons/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+    <div class="text-center text-muted">© 2017 Comic　encer.</div>
+</footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,18 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+@extends('layouts.app')
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
+@section('cover')
+    <div class="cover">
+        <div class="cover-inner">
+            <div class="cover-contents">
+                <h1>新たな物語に出会う</h1>
+                @if (!Auth::check())
+                    <a href="{{ route('signup.get') }}" class="btn btn-success btn-lg">Join Comicencer</a>
+                @endif
             </div>
         </div>
-    </body>
-</html>
+    </div>
+@endsection
+
+@section('content')
+    テスト
+@endsection
